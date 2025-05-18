26fee1e31a5c5964fa401a9faa8ac93f3ac26eed
Furk4nBulut <furkanbtng@gmail.com>
2025-05-18 19:04:50 +0300

    DBContext setted to migration.

--- YurttaYe.Data/AppDbContext.cs
+++ YurttaYe.Data/AppDbContext.cs
@@ -1,6 +1,32 @@
-namespace YurttaYe.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using YurttaYe.Core.Models.Entities;
 
-public class AppDbContext
+namespace YurttaYe.Data
 {
-    
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Menu>()
+                .HasMany(m => m.Items)
+                .WithOne(i => i.Menu)
+                .HasForeignKey(i => i.MenuId);
+
+            modelBuilder.Entity<Menu>()
+                .HasOne(m => m.City)
+                .WithMany()
+                .HasForeignKey(m => m.CityId);
+        }
+    }
 }
