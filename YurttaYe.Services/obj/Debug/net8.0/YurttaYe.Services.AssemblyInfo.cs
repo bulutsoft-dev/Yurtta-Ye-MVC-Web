f8f7b202b8791b69601a77277ab35e30ca35b37a
Furk4nBulut <furkanbtng@gmail.com>
2025-07-21 15:09:57 +0300

    Start/End time color fixed.

--- YurttaYe.Services/obj/Debug/net8.0/YurttaYe.Services.AssemblyInfo.cs
+++ YurttaYe.Services/obj/Debug/net8.0/YurttaYe.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YurttaYe.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5f4ba1e73e1051c2fb0e2a18117d10161ada98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a12dfc767a7ae2ba461a8e2553735bac0e2baa16")]
 [assembly: System.Reflection.AssemblyProductAttribute("YurttaYe.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YurttaYe.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- YurttaYe.Web/obj/Debug/net8.0/YurttaYe.Web.AssemblyInfo.cs
+++ YurttaYe.Web/obj/Debug/net8.0/YurttaYe.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YurttaYe.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5f4ba1e73e1051c2fb0e2a18117d10161ada98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a12dfc767a7ae2ba461a8e2553735bac0e2baa16")]
 [assembly: System.Reflection.AssemblyProductAttribute("YurttaYe.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YurttaYe.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
