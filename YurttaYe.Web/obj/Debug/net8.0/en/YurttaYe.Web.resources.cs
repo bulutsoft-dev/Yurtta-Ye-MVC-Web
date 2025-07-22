1326256678be5d14d6d72fed037ef01e04fcdfba
Furk4nBulut <furkanbtng@gmail.com>
2025-07-22 16:00:25 +0300

    New login screen and localization.

--- YurttaYe.Web/obj/Debug/net8.0/en/YurttaYe.Web.resources.cs
+++ YurttaYe.Web/obj/Debug/net8.0/en/YurttaYe.Web.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YurttaYe.Web")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a966298a4ac9f94d79588d403b206e4c271d6575")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15286149da1317f0f9065311da936a5aafd48a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("YurttaYe.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YurttaYe.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
