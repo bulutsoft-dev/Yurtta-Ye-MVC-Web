84619692b3f9bc3b4050719ca33874c24a135fc7
Furkan Bulut <furkanbtng@gmail.com>
2026-01-01 18:25:17 +0300

    feat: Update application startup configuration in Program.cs and rebuild projects.

--- YurttaYe.Web/obj/Debug/net8.0/en/YurttaYe.Web.resources.cs
+++ YurttaYe.Web/obj/Debug/net8.0/en/YurttaYe.Web.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YurttaYe.Web")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da18fe235e2e715847924015b716208edb096e00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9426889e3a1dec35d710b936a8359da2933a546b")]
 [assembly: System.Reflection.AssemblyProductAttribute("YurttaYe.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YurttaYe.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
